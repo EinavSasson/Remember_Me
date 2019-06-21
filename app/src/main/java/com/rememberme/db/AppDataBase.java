a4176ea9d7cb9d067b93f63eded27828c4cdab17
EINAV\einavsa <einav.sasson@gmail.com>
2019-06-21 22:32:45 +0300

    ADDING: AppDataBase

---
+++ app/src/main/java/com/rememberme/db/AppDataBase.java
@@ -0,0 +1,29 @@
+package com.rememberme.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.rememberme.model.Reminder;
+
+@Database(entities = {Reminder.class}, version = 1)
+
+public abstract class AppDataBase extends RoomDatabase {
+    public static final String DATABASE_NAME = "reminders";
+    private static AppDataBase INSTANCE;
+    public abstract ReminderDao mReminderDao();
+
+    public static AppDataBase getInstance(Context context){
+        if (INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    AppDataBase.class, DATABASE_NAME)
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return INSTANCE;
+    }
+    public static void destroyInstance(){
+        INSTANCE = null;
+    }
+}
