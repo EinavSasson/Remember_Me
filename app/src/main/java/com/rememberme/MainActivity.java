7cef43370aaa10618cd68b128f0d64dac225c0ce
EINAV\einavsa <einav.sasson@gmail.com>
2019-06-21 23:07:30 +0300

    ADDING: recyclerView for reminders list in MainActivity

--- app/src/main/java/com/rememberme/MainActivity.java
+++ app/src/main/java/com/rememberme/MainActivity.java
@@ -2,14 +2,24 @@ package com.rememberme;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.rememberme.adapter.RemindersAdapter;
+import com.rememberme.db.AppDataBase;
+import com.rememberme.model.Reminder;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, RemindersAdapter.ReminderAdapterInteraction {
+    private RemindersAdapter mRemindersAdapter;
+    private FloatingActionButton mFloatingActionButton;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +27,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setViews();
+        setRecyclerView();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        mFloatingActionButton.setOnClickListener(this);
+
     }
+    private void setViews() {
+        mFloatingActionButton = findViewById(R.id.fab);
+    }
+
+    private void setRecyclerView() {
+        mRecyclerView  = findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRemindersAdapter = new RemindersAdapter(this, getReminderList());
+    }
+    private List<Reminder> getReminderList(){
+        return AppDataBase.getInstance(this).mReminderDao().getAll();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab:
+                break;
+        }
+
+    }
+
+    @Override
+    public void onDeleteReminder(Reminder reminder) {
+        AppDataBase.getInstance(this).mReminderDao().delete(reminder);
     }
 }
