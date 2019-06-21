6c8b5bdb911e3930119218c1ba968b055e59db9e
EINAV\einavsa <einav.sasson@gmail.com>
2019-06-21 23:11:46 +0300

    ADDING: dialog fragment class for create a new reminder

--- app/src/main/java/com/rememberme/fragment/AddReminderDialogFragment.java
+++ app/src/main/java/com/rememberme/fragment/AddReminderDialogFragment.java
@@ -1,4 +1,21 @@
 package com.rememberme.fragment;
 
-public class AddReminderDialogFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rememberme.R;
+
+public class AddReminderDialogFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.dialog_botton_sheet_add_reminder,container, false);
+       return view;
+    }
 }
