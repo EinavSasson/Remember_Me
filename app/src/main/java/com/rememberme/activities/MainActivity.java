30a3307618abc39cc25b44898873d2fc914ddcdb
EINAV\einavsa <einav.sasson@gmail.com>
2019-07-03 22:45:28 +0300

    ADDING: onAddReminderClick interface for get data from fragment dialog

--- app/src/main/java/com/rememberme/activities/MainActivity.java
+++ app/src/main/java/com/rememberme/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rememberme;
+package com.rememberme.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,16 +10,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.rememberme.R;
 import com.rememberme.adapter.RemindersAdapter;
 import com.rememberme.db.AppDataBase;
+import com.rememberme.fragment.AddReminderDialogFragment;
 import com.rememberme.model.Reminder;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, RemindersAdapter.ReminderAdapterInteraction {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, RemindersAdapter.ReminderAdapterInteraction, AddReminderDialogFragment.AddReminderListener {
     private RemindersAdapter mRemindersAdapter;
     private FloatingActionButton mFloatingActionButton;
     private RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +31,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setViews();
+        onClickListener();
         setRecyclerView();
-
-
-        mFloatingActionButton.setOnClickListener(this);
-
     }
     private void setViews() {
         mFloatingActionButton = findViewById(R.id.fab);
+
+    }
+    private void onClickListener(){
+        mFloatingActionButton.setOnClickListener(this);
     }
 
     private void setRecyclerView() {
@@ -43,11 +47,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(linearLayoutManager);
         mRemindersAdapter = new RemindersAdapter(this, getReminderList());
+        mRecyclerView.setAdapter(mRemindersAdapter);
     }
     private List<Reminder> getReminderList(){
         return AppDataBase.getInstance(this).mReminderDao().getAll();
     }
-
+    private Reminder createReminder(String reminderText, String noteText){
+        Reminder reminder = new Reminder();
+        reminder.setTitleReminder(reminderText);
+        reminder.setNoteReminder(noteText);
+        return reminder;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,7 +71,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -69,13 +78,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.fab:
+                AddReminderDialogFragment.newInstance().show(getSupportFragmentManager(), "add_reminder");
                 break;
         }
-
     }
 
     @Override
     public void onDeleteReminder(Reminder reminder) {
         AppDataBase.getInstance(this).mReminderDao().delete(reminder);
+    }
+
+    @Override
+    public void onUpdateReminder(Reminder reminder) {
+        AppDataBase.getInstance(this).mReminderDao().update(reminder);
+    }
+
+    @Override
+    public void onAddReminderClick(String reminderText, String noteText) {
+        Reminder reminder =createReminder(reminderText, noteText);
+        AppDataBase.getInstance(this).mReminderDao().insert(reminder);
+        List<Reminder>updateReminderList = getReminderList();
+        mRemindersAdapter.updateList(updateReminderList);
     }
 }
