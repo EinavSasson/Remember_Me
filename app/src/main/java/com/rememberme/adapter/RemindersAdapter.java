62eecb8e625b0841774d4de04a7ea658044e63f0
EINAV\einavsa <einav.sasson@gmail.com>
2019-07-08 18:36:36 +0300

    ADDING: delete item method

--- app/src/main/java/com/rememberme/adapter/RemindersAdapter.java
+++ app/src/main/java/com/rememberme/adapter/RemindersAdapter.java
@@ -1,7 +1,9 @@
 package com.rememberme.adapter;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,25 +55,47 @@ public class RemindersAdapter extends RecyclerView.Adapter<RemindersAdapter.View
     notifyItemRangeChanged(position, mReminderList.size());
     }
 
+
+
     public void updateList(List<Reminder> updateList){
         mReminderList = updateList;
         notifyDataSetChanged();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public void deleteItem(int position){
+
+     //   int position = (int) v.getTag();
+        Reminder reminder = mReminderList.get(position);
+        mListener.onDeleteReminder(reminder);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mReminderList.size());
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
         public final View mView;
         public final TextView tvTitle;
         public final TextView tvNote;
+        public final CardView mCardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mView = itemView;
             tvTitle = itemView.findViewById(R.id.titleReminder);
             tvNote = itemView.findViewById(R.id.noteReminder);
+            mCardView = itemView.findViewById(R.id.card_view_reminder);
+            mCardView.setOnCreateContextMenuListener(this);
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.add(this.getAdapterPosition(), 1, 0, "Delete");
+
         }
     }
     public interface ReminderAdapterInteraction{
         void onDeleteReminder(Reminder reminder);
-        void onUpdateReminder(Reminder reminder);
     }
+
+
 }
