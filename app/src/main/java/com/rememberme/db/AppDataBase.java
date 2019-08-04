cfdfe7c265c0eb5c914047ae737cf31b44ea5ed6
EINAV\einavsa <einav.sasson@gmail.com>
2019-08-04 14:46:18 +0300

    UPDATE: new version.

--- app/src/main/java/com/rememberme/db/AppDataBase.java
+++ app/src/main/java/com/rememberme/db/AppDataBase.java
@@ -3,12 +3,14 @@ package com.rememberme.db;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
+import com.rememberme.model.DateConverter;
 import com.rememberme.model.Reminder;
 
-@Database(entities = {Reminder.class}, version = 1)
-
+@Database(entities = {Reminder.class}, version = 2, exportSchema = false)
+@TypeConverters(DateConverter.class)
 public abstract class AppDataBase extends RoomDatabase {
     public static final String DATABASE_NAME = "reminders";
     private static AppDataBase INSTANCE;
